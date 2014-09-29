d6d7bd471493144a07d2d7ef0c4bd9016f46bc0a
moshek <mkolodny@integralads.com>
2014-09-29 18:08:18 -0400

    added beforeEach

--- test/index.js
+++ test/index.js
@@ -1,6 +1,22 @@
+beforeEach(function() {
+  console.log('wont work')
+})
 describe('a test', function() {
+  beforeEach(function() {
+    console.log('inside beforeEach')
+  })
   it('is a test', function(done) {
-    console.log(123);
+    console.log(9);
+    setTimeout(function() {
+      console.log(234);
+      //throw new Error(42);
+      done();
+    }, 1500);
+
+  });
+
+  it('is also test', function(done) {
+    console.log(8);
     setTimeout(function() {
       console.log(234);
       //throw new Error(42);
@@ -12,7 +28,7 @@ describe('a test', function() {
 
 describe('a test2', function() {
   it('is a test2', function(done) {
-    console.log(123);
+    console.log(1234);
     setTimeout(done, 1500);
 
   })
